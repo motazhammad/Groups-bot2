14cc113cd38f331a9cc2bf74d2f0e54d8c63d5da
Yosef-lbban <yosef.lbban@gmail.com>
2021-10-16 23:23:36 +0100

    Update yousef.lua

--- yousef.lua
+++ yousef.lua
@@ -1669,22 +1669,26 @@ end
 return false
 end
 database:set(bot_id.."yousef:Lock:Link"..msg.chat_id_,"del")  
-Reply_Status(msg,msg.sender_user_id_,"lock","• تم قفـل الروابط")  
+Reply_Status(msg,msg.sender_user_id_,"lock","• تم قفـل الروابط
+. هذا يعني أن نشر الروابط ممنوع")  
 return false
 end 
 if text == "قفل الروابط بالتقيد" and Addictive(msg) then
 database:set(bot_id.."yousef:Lock:Link"..msg.chat_id_,"ked")  
-Reply_Status(msg,msg.sender_user_id_,"lockkid","• تم قفـل الروابط")  
+Reply_Status(msg,msg.sender_user_id_,"lockkid","• تم قفـل الروابط
+. هذا يعني أن نشر الروابط ممنوع")  
 return false
 end 
 if text == "قفل الروابط بالكتم" and Addictive(msg) then
 database:set(bot_id.."yousef:Lock:Link"..msg.chat_id_,"ktm")  
-Reply_Status(msg,msg.sender_user_id_,"lockktm","• تم قفـل الروابط")  
+Reply_Status(msg,msg.sender_user_id_,"lockktm","• تم قفـل الروابط
+. هذا يعني أن نشر الروابط ممنوع")  
 return false
 end 
 if text == "قفل الروابط بالطرد" and Addictive(msg) then
 database:set(bot_id.."yousef:Lock:Link"..msg.chat_id_,"kick")  
-Reply_Status(msg,msg.sender_user_id_,"lockkick","• تم قفـل الروابط")  
+Reply_Status(msg,msg.sender_user_id_,"lockkick","• تم قفـل الروابط
+. هذا يعني أن نشر الروابط ممنوع")  
 return false
 end 
 if text == "فتح الروابط" and Addictive(msg) then  
